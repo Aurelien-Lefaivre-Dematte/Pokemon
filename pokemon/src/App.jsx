1b9f15cf58f6414b6a09de60117f73c86d010162
Aurélien Lefaivre Dematte <127293846+Aurelien-Lefaivre-Dematte@users.noreply.github.com>
2023-07-19 20:43:50 +0200

    avec photo

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -19,12 +19,21 @@ function App() {
   }, []);
 
   return (
+    <>
     <ul>
-      {posts.map(pokemon => (
-        console.log(pokemon),
-        <li key={pokemon.pokedexId}>{pokemon.pokedexId}</li>
-      ))}
-    </ul>
+        {posts.map(pokemon => (
+          <div key={pokemon.pokedexId}>
+            <img src={pokemon.sprites.regular} alt="" />
+            <li>Nom Français: {pokemon.name.fr}</li>
+            <p>Nom Japonais: {pokemon.name.jp}</p>
+            <p>Numéro dans le pokedex: {pokemon.pokedexId}</p>
+            <p>Génération: {pokemon.generation}</p>
+            {/* Console.log a été supprimé de JSX */}
+            {/* <li>{console.log(pokemon.types)}</li> */}
+          </div>
+        ))}
+      </ul>
+    </>
   );
 }
 
